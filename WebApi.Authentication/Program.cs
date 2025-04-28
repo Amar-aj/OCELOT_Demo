185e270596fd878a32e36700b1b9b9f708d074b5
amar-tatwa <amarjyoti.mahanta@tatwa.info>
2025-04-28 11:42:32 +0530

    update for all route access

--- WebApi.Authentication/Program.cs
+++ WebApi.Authentication/Program.cs
@@ -35,6 +35,20 @@ app.MapGet("/auth/weatherforecast", () =>
 })
 .WithName("Auth GetWeatherForecast")
 .WithOpenApi();
+app.MapGet("/auth/demo-weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("Auth Demo GetWeatherForecast")
+.WithOpenApi();
 
 app.Run();
 
